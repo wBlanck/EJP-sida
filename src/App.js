a6adc660bba5dccb580f90ff1d8ade97b992c197
wblanck <blanck.william@gmail.com>
2022-09-05 04:26:04 +0200

    started on navbar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <NavBar />
-    </div>
+      <div className="App"></div>
+    </>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,19 @@
+import { AiOutlineFacebook, AiOutlineInstagram } from "react-icons/ai";
+
 const NavBar = () => {
-  return <div className="">hello</div>;
+  return (
+    <nav className="">
+      <div className="">
+        <a href="https//google.se">
+          <AiOutlineFacebook />
+        </a>
+
+        <a href="https//google.se">
+          <AiOutlineInstagram />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
