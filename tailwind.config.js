af801fd8296662921ccdce71fd5eaa7747659fea
wblanck <blanck.william@gmail.com>
2022-09-06 05:14:46 +0200

    custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "home-bg": "url(assets/homebg.jpg)",
+      },
       container: {
         "2xl": "1366px",
       },
+      maxWidth: {},
     },
   },
   plugins: [],
