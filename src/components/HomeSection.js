7fee3b3db320a2633713ca167fb734cb5d9cfbbb
wblanck <blanck.william@gmail.com>
2022-09-07 04:31:28 +0200

    adjusted font sizes for mobile

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -5,8 +5,10 @@ const HomeSection = () => {
     <div className="h-screen bg-center bg-no-repeat bg-cover bg-home-bg">
       <section className="container flex items-center justify-center h-full mx-auto max-w-7">
         <div className="text-center text-white">
-          <h1 className="mb-6 text-5xl font-medium">EJP Bygg AB</h1>
-          <h2 className="text-3xl">Snickare med känsla för detaljer</h2>
+          <h1 className="mb-6 text-4xl font-medium lg:text-5xl">EJP Bygg AB</h1>
+          <h2 className="text-xl lg:text-3xl">
+            Snickare med känsla för detaljer
+          </h2>
         </div>
       </section>
     </div>
