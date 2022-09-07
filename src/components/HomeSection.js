fca890d28e080794387c89bc91ddcd516c58152d
wblanck <blanck.william@gmail.com>
2022-09-07 05:35:06 +0200

    homepage: adjusted font size on larger screens

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -5,8 +5,8 @@ const HomeSection = () => {
     <div className="h-screen bg-center bg-no-repeat bg-cover bg-home-bg">
       <section className="container flex items-center justify-center h-full mx-auto max-w-7">
         <div className="text-center text-white">
-          <h1 className="mb-6 text-4xl font-medium lg:text-5xl">EJP Bygg AB</h1>
-          <h2 className="text-xl lg:text-3xl">
+          <h1 className="mb-6 text-4xl font-medium xl:text-5xl">EJP Bygg AB</h1>
+          <h2 className="text-xl xl:text-3xl">
             Snickare med känsla för detaljer
           </h2>
         </div>
