9ef6102b27a9fbdd9ca980c2222f2c374f65ee61
wblanck <blanck.william@gmail.com>
2022-09-14 18:00:02 +0200

    styled menu

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,32 +1,30 @@
 import React from "react";
-import { AiOutlineClose } from "react-icons/ai";
 
 const MobileMenu = () => {
   return (
-    <div className="fixed flex flex-col items-center justify-center w-screen h-screen bg-slate-400">
-      <AiOutlineClose size={34} className="self-end" />
-      <ul className="w-full p-8">
+    <div className="fixed top-0 left-0 flex flex-col justify-center w-screen h-screen bg-[#232323] -z-10 text-white">
+      <ul className="flex flex-col w-full h-full gap-4 p-8 justify-evenly">
         <li className="grid grid-cols-2">
-          <span className="grid justify-start text-2xl">01</span>
-          <a href="" className="justify-self-end">
+          <span className="grid justify-start text-lg text-orange-600">01</span>
+          <a href="" className="text-lg justify-self-end">
             Startsida
           </a>
         </li>
         <li className="grid grid-cols-2">
-          <span className="grid justify-center text-2xl">02</span>
-          <a href="" className="justify-self-end">
+          <span className="grid justify-start text-lg text-orange-600">02</span>
+          <a href="" className="text-lg justify-self-end">
             Om Oss
           </a>
         </li>
         <li className="grid grid-cols-2">
-          <span className="grid justify-center text-2xl">03</span>
-          <a href="" className="justify-self-end">
+          <span className="grid justify-start text-lg text-orange-600">03</span>
+          <a href="" className="text-lg justify-self-end">
             Kontakta Oss
           </a>
         </li>
         <li className="grid grid-cols-2">
-          <span className="grid justify-center text-2xl">04</span>
-          <a href="" className="justify-self-end">
+          <span className="grid justify-start text-lg text-orange-600">04</span>
+          <a href="" className="text-lg justify-self-end">
             Portfolio
           </a>
         </li>
