515f8057b0f7c2a20c7374818b450ef1d155816c
wblanck <blanck.william@gmail.com>
2022-09-16 08:16:36 +0200

    redesigned about slider buttons

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -15,7 +15,7 @@ const AboutSection = () => {
         <div className="absolute left-0 bg-[#232323] h-[500px] w-4/5 -z-0 hidden lg:block"></div>
         <div className="relative flex flex-col w-full gap-5 mx-auto text-center text-white max-w-7xl lg:text-start">
           <h2 className="text-2xl font-medium ">EJP Bygg AB</h2>
-          <p className="max-w-xl mx-auto lg:text-l lg:ml-0 lg:max-w-sm">
+          <p className="max-w-xl mx-auto lg:text-l lg:ml-0 lg:max-w-md">
             Vi är ett traditionellt byggföretag som värnar om detaljerna. Med
             lokala leverantörer som vi har samarbete med, så ger vi kunden en
             slutprodukt som ni kan vara stolt över. Vi utför olika projekt,
@@ -23,18 +23,11 @@ const AboutSection = () => {
             <span className="text-orange-600"> renovering</span> både små och
             större projekt!
           </p>
-          <button type="button" className="italic mb-7 lg:self-start">
-            Kontakta Oss
+
+          <button className="flex items-center justify-center gap-3 lg:self-start lg:w-2/3 lg:gap-10 lg:justify-start">
+            <span className="text-lg">Läs Mer</span>
+            <AiOutlineArrowRight className="text-2xl lg:text-xl" />
           </button>
-          <div className="flex items-center justify-center gap-3 lg:self-start lg:w-2/3 lg:gap-10">
-            <button>
-              <AiOutlineArrowLeft className="text-3xl border rounded-full lg:text-4xl" />
-            </button>
-            <span className="text-lg lg:order-1 lg:ml-auto">1 / 2</span>
-            <button>
-              <AiOutlineArrowRight className="text-3xl border rounded-full lg:text-4xl" />
-            </button>
-          </div>
         </div>
       </section>
     </div>
