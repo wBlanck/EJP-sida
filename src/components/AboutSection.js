d9fb385b810aa105559a6a13247f2419050d56a0
wblanck <blanck.william@gmail.com>
2022-09-16 07:36:10 +0200

    adjusted for smaller screens

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,16 +3,16 @@ import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 const AboutSection = () => {
   return (
-    <div className="bg-[#1f1f1f] pt-24 relative lg:h-screen">
-      <div className="h-[400px] w-full lg:h-[450px] lg:w-[50%] relative lg:absolute lg:right-10  lg:top-10 z-[2]">
+    <div className="bg-[#1f1f1f] pt-24 relative lg:h-screen flex flex-col gap-10">
+      <div className="h-[400px] w-full lg:h-[450px] lg:w-[50%] relative lg:absolute lg:right-10 lg:top-10 z-[2]">
         <img
           src={aboutOne}
           alt=""
           className="block object-cover w-full h-full"
         />
       </div>
-      <section className="flex items-center w-full h-full mx-auto">
-        <div className="absolute left-0 bg-[#232323] h-[500px] w-4/5 -z-0"></div>
+      <section className="flex items-center w-full h-full p-4 mx-auto">
+        <div className="absolute left-0 bg-[#232323] h-[500px] w-4/5 -z-0 hidden lg:block"></div>
         <div className="relative flex flex-col w-full gap-5 mx-auto text-center text-white max-w-7xl lg:text-start">
           <h2 className="text-2xl font-medium ">EJP Bygg AB</h2>
           <p className="max-w-xl mx-auto lg:text-l lg:ml-0 lg:max-w-sm">
